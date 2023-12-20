0ed2e228af95aaaf643fb89f23c62d6e0eef2380
rifkkimaulana <rifkkimaulana@gmail.com>
2023-12-20 10:31:12 +0700

    new import auth

---
+++ vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'de4878a029097e4f51297d168fb4c5aaf8ead654',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'de4878a029097e4f51297d168fb4c5aaf8ead654',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'agungsugiarto/codeigniter4-cors' => array(
+            'pretty_version' => 'v1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '75d136b1e2349e76fc3641e024714a77e2fbf07d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../agungsugiarto/codeigniter4-cors',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeigniter4/authentication-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'myth/auth' => array(
+            'pretty_version' => 'v1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => 'c62802b9e2ce5be931f15270811d831cc2e85457',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../myth/auth',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
